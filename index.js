a8992c08bb81eb9a7499530801c18d8d57eb510d
Stanislav Dryomov <stasundr@gmail.com>
2017-11-02 03:54:27 +0700

    common/signature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const common = require('./lib/common');
 const ticker = require('./lib/ticker');
 const transactions = require('./lib/transactions');
 
+module.exports.Common = common;
 module.exports.Ticker = ticker;
 module.exports.Transactions = transactions;
