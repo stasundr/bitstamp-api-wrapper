f2e4d35f9681298a2102a5776a66608533275469
Stanislav Dryomov <stasundr@gmail.com>
2017-11-05 23:57:58 +0700

    transactions; balance

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const log = require('intel').getLogger('Bitstamp');
 const Signature = require('./lib/signature');
 const Ticker = require('./lib/ticker');
 const Transactions = require('./lib/transactions');
+const Private = require('./lib/private');
 
 // 600 requests max per 10 minutes
 const MAX_CALL_WINDOW = 60 * 1000; // 10 minutes
@@ -11,9 +12,7 @@ const MAX_CALLS_PER_WINDOW = 60;
 
 class Bitstamp {
     constructor() {
-        this.totalCallsMade = 0;
         this.callsInLastMinute = 0;
-        this.lastCall = null;
         this.signature = new Signature();
 
         this._intv = setInterval(() => {
@@ -21,16 +20,49 @@ class Bitstamp {
         }, MAX_CALL_WINDOW);
     }
 
+    _registerCall() {
+        this.callsInLastMinute++;
+
+        if (this.callsInLastMinute >= MAX_CALLS_PER_WINDOW) {
+            log.error(new Error(`Must not exceed ${MAX_CALLS_PER_WINDOW} calls per ${MAX_CALL_WINDOW} ms.`));
+        }
+    }
+
+    close() {
+        if (this._intv) {
+            clearInterval(this._intv);
+        }
+    }
+
     // PUBLIC DATA FUNCTIONS
     async ticker(currencyPair) {
+        this._registerCall();
         return await Ticker.ticker(currencyPair);
     }
 
     async getCurrentPrice(currencyPair) {
+        this._registerCall();
         return await Ticker.getCurrentPrice(currencyPair);
     }
 
+    async transactions(currencyPair) {
+        this._registerCall();
+        return await Transactions.list(currencyPair);
+    }
+
     // PRIVATE FUNCTIONS
+    async balance(currencyPair) {
+        this._registerCall();
+        return await Private.balance.bind(this)(currencyPair);
+    }
 }
 
 module.exports = Bitstamp;
+
+const B = new Bitstamp();
+B.balance()
+    .then(x => console.log(x))
+    .catch(e => {
+        console.log('bivaet');
+    });
+B.close();
