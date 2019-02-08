7620d2b1d512d41a2ef269139293a3c915576ed6
stasundr <stasundr@gmail.com>
2019-02-08 12:27:04 +0700

    fix lodash vulnerability

--- index.js
+++ index.js
@@ -1,65 +1,63 @@
-const Promise = require('bluebird');
-const request = require('superagent');
-const log = require('intel').getLogger('Bitstamp');
-const Signature = require('./lib/signature');
-const Ticker = require('./lib/ticker');
-const Transactions = require('./lib/transactions');
-const Private = require('./lib/private');
+const log = require('intel').getLogger('Bitstamp')
+const Signature = require('./lib/signature')
+const Ticker = require('./lib/ticker')
+const Transactions = require('./lib/transactions')
+const Private = require('./lib/private')
 
 // 600 requests max per 10 minutes
-const MAX_CALL_WINDOW = 60 * 1000; // 10 minutes
-const MAX_CALLS_PER_WINDOW = 60;
+const MAX_CALL_WINDOW = 60 * 1000 // 10 minutes
+const MAX_CALLS_PER_WINDOW = 60
 
 class Bitstamp {
-    constructor(key, secret, clientId) {
-        this.callsInLastMinute = 0;
-        this.signature = new Signature(key, secret, clientId);
+  constructor(key, secret, clientId) {
+    this.callsInLastMinute = 0
+    this.signature = new Signature(key, secret, clientId)
 
-        this._intv = setInterval(() => {
-            this.callsInLastMinute = 0;
-        }, MAX_CALL_WINDOW);
+    this._intv = setInterval(() => {
+      this.callsInLastMinute = 0
+    }, MAX_CALL_WINDOW)
+  }
+
+  _registerCall() {
+    this.callsInLastMinute++
+
+    if (this.callsInLastMinute >= MAX_CALLS_PER_WINDOW) {
+      log.error(new Error(`Must not exceed ${MAX_CALLS_PER_WINDOW} calls per ${MAX_CALL_WINDOW} ms.`))
     }
+  }
 
-    _registerCall() {
-        this.callsInLastMinute++;
-
-        if (this.callsInLastMinute >= MAX_CALLS_PER_WINDOW) {
-            log.error(new Error(`Must not exceed ${MAX_CALLS_PER_WINDOW} calls per ${MAX_CALL_WINDOW} ms.`));
-        }
+  close() {
+    if (this._intv) {
+      clearInterval(this._intv)
     }
+  }
 
-    close() {
-        if (this._intv) {
-            clearInterval(this._intv);
-        }
-    }
+  // PUBLIC DATA FUNCTIONS
+  async ticker(currencyPair) {
+    this._registerCall()
+    return await Ticker.ticker(currencyPair)
+  }
 
-    // PUBLIC DATA FUNCTIONS
-    async ticker(currencyPair) {
-        this._registerCall();
-        return await Ticker.ticker(currencyPair);
-    }
+  async getCurrentPrice(currencyPair) {
+    this._registerCall()
+    return await Ticker.getCurrentPrice(currencyPair)
+  }
 
-    async getCurrentPrice(currencyPair) {
-        this._registerCall();
-        return await Ticker.getCurrentPrice(currencyPair);
-    }
+  async transactions(currencyPair, time) {
+    this._registerCall()
+    return await Transactions.list(currencyPair, time)
+  }
 
-    async transactions(currencyPair, time) {
-        this._registerCall();
-        return await Transactions.list(currencyPair, time);
-    }
+  // PRIVATE FUNCTIONS
+  async balance(currencyPair) {
+    this._registerCall()
+    return await Private.balance.bind(this)(currencyPair)
+  }
 
-    // PRIVATE FUNCTIONS
-    async balance(currencyPair) {
-        this._registerCall();
-        return await Private.balance.bind(this)(currencyPair);
-    }
-
-    async userTransactions(currencyPair, offset, limit) {
-        this._registerCall();
-        return await Private.userTransactions.bind(this)(currencyPair, offset, limit);
-    }
+  async userTransactions(currencyPair, offset, limit) {
+    this._registerCall()
+    return await Private.userTransactions.bind(this)(currencyPair, offset, limit)
+  }
 }
 
-module.exports = Bitstamp;
+module.exports = Bitstamp
